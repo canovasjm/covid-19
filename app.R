919b56cacb2be662274aee1b0b1cd9bcf30b3434
jmcanovas <juanmacanovas@gmail.com>
2020-08-01 19:07:35 -0300

    change shiny app to read processed data from GitHub repository

--- app.R
+++ app.R
@@ -3,26 +3,8 @@ library(tidyverse)
 library(shiny)
 library(plotly)
 
-# read data ---------------------------------------------------------------
-df <- read_csv(file = "https://www.gstatic.com/covid19/mobility/Global_Mobility_Report.csv?cachebust=722f3143b586a83f")
-
-# transform to tidy data
-df <- df %>% 
-  filter(country_region_code == "AR") %>% 
-  select(country_region, 
-         sub_region_1, 
-         date, 
-         retail_and_recreation_percent_change_from_baseline, 
-         grocery_and_pharmacy_percent_change_from_baseline,
-         parks_percent_change_from_baseline, 
-         transit_stations_percent_change_from_baseline, 
-         workplaces_percent_change_from_baseline, 
-         residential_percent_change_from_baseline) %>% 
-  pivot_longer(-c(country_region, sub_region_1, date), names_to = "type", values_to = "values") %>% 
-  mutate(sub_region_1 = sub("Buenos Aires$", "CABA", sub_region_1)) %>% 
-  mutate(sub_region_1 = sub("* Province", "", sub_region_1)) %>%
-  mutate(sub_region_1 = replace_na(sub_region_1, "Todas las provincias")) %>% 
-  mutate(type = sub("_percent_change_from_baseline$", "", type))
+# read processed data from my GitHub repository ---------------------------
+df <- read_csv(file = "https://raw.githubusercontent.com/canovasjm/covid-19/master/data/google_mobility_report.csv")
 
 # set system locale to spanish in order to show days ----------------------
 # Sys.getlocale("LC_TIME")
